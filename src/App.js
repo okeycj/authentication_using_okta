1919669a2ab1357315d737e622fbe4108bcce400
Azubuike Chijindu Okechukwu <30808024+okeycj@users.noreply.github.com>
2019-11-28 15:58:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
+import Navbar from "./components/layouts/Navbar"
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
+
 import './App.css';
+
+function onAuthRequired({history}) {
+  history.push('/login');
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Security issuer="https://dev-230867.okta.com/oauth2/default"
+                client_id="0oa1zf29zqfatIJUE357"
+                redirect_uri={window.location.origin + '/implicit/callback'}
+                onAuthRequired={onAuthRequired}>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <SecureRoute path="/staff" exact={true} component={Staff} />
+            <Route path="/Login" render={() => <Login
+            baseUrl='https://dev-230867.okta.com' />} />
+            <Route path='/implicit/callback' component={ImplicitCallback} />
+          </div>
+        </div>
+      </Security>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
+
+export default withAuth(
+  class Home extends Component {
+    state = { authenticated: null };
+
+  checkAuthentication = async() =>  {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
+    }
+  }
+
+  async componentDidMount() {
+    this.checkAuthentication();
+  }
+
+  async componentDidUpdate() {
+    this.checkAuthentication();
+  }
+
+  login = async() => {
+    this.props.auth.login('/');
+  }
+
+  logout = async() => {
+    this.props.auth.logout('/');
+  }
+
+  render() {
+    if (this.state.authenticated === null) return null;
+
+    const mainContent = this.state.authenticated ? (
+        <div>
+            <p className="lead">You Have Entered The Staff Portal, <Link to="/staff">Click here</Link></p>
+            <button className="btn btn-light btn-lg" onClick={this.logout}>Logout</button>;
+        </div>
+    ) : (
+      <div>
+          <p className="lead">if you are a staff member please get your credientials from your supervisor</p>
+          <button className="btn btn-light btn-lg" onClick={this.login}>Login</button>
+      </div>
+    );
+    return (
+      <div className="jumbotron">
+        <h1 className="display-4">Acme Staff Portal</h1>
+        {mainContent}
+      </div>
+    );
+  }
+});
